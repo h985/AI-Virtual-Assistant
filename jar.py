401b7e9f70f4e02ce0a75efda50ccec88c0e37ce
h985 <61300461+h985@users.noreply.github.com>
2020-10-10 13:32:54 +0530

    Update jar.py

--- jar.py
+++ jar.py
@@ -86,6 +86,24 @@ def check_internet_connection():
         pass
     return False
                    
+ def getDate():
+    now = datetime.datetime.now()
+    my_date =datetime.datetime.today()
+    weakday = calendar.day_name[my_date.weekday()]
+    monthNum = now.month
+    dayNum = now.day
+
+    MONTHS = ['January', 'February' , 'March' , 'April' , 'May' , 'June' ,
+                   'July' , 'August' , 'September' , 'October' , 'November' ,'December']
+
+    ordinalNumbers = ['1st' , '2nd' , '3rd' , '4th' , '5th ', '6th' , '7th' , '8th' , '9th' , '10th' , '11th',
+                      '12th' , '13th' , '14th' , '15th' , '16th' , '17th' ,'18th' , '19th' , '20th' , '21st' ,
+                      '22nd', '23rd' , '24th' , '25th' , '26th', '27th' , '28th' , '29th' , '30th' , '31st'
+                     ]
+
+    return 'Today is {} of {} {}'.format(ordinalNumbers[dayNum -1],MONTHS[monthNum -1],weakday)
+
+                   
 def changePassword():
         pword = getpass('\n\tEnter New Password : ')
         print('\n\tPassword Updated Successfully!')
@@ -842,8 +860,7 @@ if __name__ == "__main__":
             speak(f"Sir, the time is {strTime}")
 
         elif "date" in query:
-            strDate = datetime.datetime.now().strftime("%D/%m/%Y")    
-            speak(f"Sir, the Date is {strDate}")
+            getDate() 
             
         elif 'who are you' in query:
             speak(f"I am jane sir ,I am your assistant who are also a freind and wants your love")
